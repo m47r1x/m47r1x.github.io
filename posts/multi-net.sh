b14a1cf03ae41f29eb0bd098e8e1841aee85eb9f
Hritik Vijay <hritikxx8@gmail.com>
2020-04-21 19:31:21 +0530

    rebuilding site Tue 21 Apr 2020 07:31:21 PM IST

--- posts/multi-net.sh
+++ posts/multi-net.sh
@@ -2,10 +2,38 @@
 # (C) Hritik Vijay
 # License: GPLv2
 
+tmp=$(mktemp multi-net-ip-XXXX --tmpdir)
+TIMEOUT=5
 Table=200
 
+_ping(){
+	echo "Wait $1 seconds "
+	init=$(date +%s)
+	curr=$init
+	until [ $(($curr-$init)) -ge "$1" ]; do
+		curl -s -m $1 "ipecho.net/plain" >> "$tmp"
+		echo  >> "$tmp"
+		echo -n .
+		curr=$(date +%s)
+	done
+	echo
+}
+
+trap "rm $tmp && rm -f $tmp.uniq" EXIT
+
+#----------------------main-------------------#
+
+# Need root
+if [ "$(id -u)" -ne 0 ]; then
+	echo "[!] Root required"
+	exit 
+fi
+
 route_cmd="ip route add default scope global "
+
+echo "[*] Scanning interfaces"
 # Iterate through each interface and process them
+count=0
 while read -r line; do
 	P=$(echo $line | grep -Eo 'via [0-9\.]+ ' | cut -d " " -f 2)
 	IF=$(echo $line | grep -Eo 'dev [A-Za-z0-9]+ ' | cut -d " " -f 2)
@@ -24,7 +52,26 @@ while read -r line; do
 
 	# a new table
 	Table=$((Table+1))
+
+	echo "[*] Processed $IF"
+	count=$((count+1))
 done < <(ip route list default)
 
-echo $route_cmd
+if [ $count -lt 2 ]; then 
+	echo "[!] Please connect at least two internet links. Aborted."
+	exit
+fi
+
+echo "[*] Creating multipath route"
 eval "$route_cmd"
+
+echo "[*] Verifying"
+_ping $TIMEOUT
+sort "$tmp" | uniq > "$tmp.uniq"
+
+if [ $(wc -l "$tmp.uniq" | cut -f 1 -d " ") -gt 1 ]; then
+	echo "[*] Success ! Your public IPs: "
+	cat "$tmp.uniq"
+else
+	echo "[!] Failed"
+fi
